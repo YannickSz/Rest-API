b98601c037effa1ac9e010217891b1ad4bae03b1
YannickSz <yannickschulz01@gmail.com>
2021-05-28 11:24:41 +0200

    added Basic auth for endpoints

--- src/main/java/de/yannickSz/Rest/API/RestAPI/configs/WebSecurityConfig.java
+++ src/main/java/de/yannickSz/Rest/API/RestAPI/configs/WebSecurityConfig.java
@@ -1,17 +1,40 @@
 package de.yannickSz.Rest.API.RestAPI.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("doge").password(passwordEncoder().encode("elSandore"))
+                .authorities("BIG_PP");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/getCustomerMail","/addCustomer").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .realmName("Rest-API");
     }
-
 }
